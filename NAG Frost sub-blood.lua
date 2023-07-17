11664b766b3d0880b2f507291b591ab1ddb1f397
Adam Kapos <Nezz@users.noreply.github.com>
2023-07-17 23:57:19 +0300

    Add frost sub-unholy rotation

---
+++ NAG Frost sub-blood.lua
@@ -0,0 +1,34 @@
+function(auras)
+    local spells = aura_env.Spells
+    aura_env.Update(auras, function()
+        return aura_env.Sequence("Opener",
+        spells.IcyTouch,
+        spells.PlagueStrike,
+        spells.UnbreakableArmor,
+        spells.BloodTap,
+        spells.Obliterate,
+        spells.FrostStrike,
+        spells.Pestilence,
+        spells.EmpowerRuneWeapon,
+        spells.Obliterate,
+        spells.FrostStrike,
+        spells.Obliterate,
+        spells.Obliterate,
+        spells.RaiseDead)
+    or (not aura_env.DotIsActive(spells.FrostFever) and aura_env.Cast(spells.IcyTouch))
+    or (not aura_env.DotIsActive(spells.BloodPlague) and aura_env.Cast(spells.PlagueStrike))
+    or (aura_env.DotRemainingTime(spells.FrostFever) < 1.5 and aura_env.Cast(spells.Pestilence))
+    or (aura_env.CanCast(spells.UnbreakableArmor) and aura_env.Cast(spells.Gloves))
+    or (aura_env.CanCast(spells.UnbreakableArmor) and aura_env.Cast(spells.PotionOfSpeed))
+    or (aura_env.Cast(spells.UnbreakableArmor) and aura_env.Cast(spells.BloodTap))
+    or (aura_env.DotRemainingTime(spells.FrostFever) < 4 and aura_env.Cast(spells.Pestilence))
+    or (aura_env.AuraIsActive(spells.Rime) and aura_env.Cast(spells.HowlingBlast))
+    or aura_env.Cast(spells.Obliterate)
+    or aura_env.Cast(spells.RaiseDead)
+    or aura_env.Cast(spells.BloodStrike)
+    or aura_env.Cast(spells.FrostStrike)
+    or aura_env.Cast(spells.HornOfWinter)
+    end)
+    return true
+end
+
